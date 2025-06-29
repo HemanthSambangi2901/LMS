75cf9b91d060862ccfe17b43a4c4e63e1625e8e8
Samhitha <samhithabuddha@gmail.com>
2025-06-30 01:13:14 +0530

    First changes in student file

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,12 +1,50 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext()
 
 export const AppContextProvider = (props)=>{
 
-    const value = {
 
+    const currency=import.meta.env.VITE_CURRENCY
+    const navigate=useNavigate();
+
+    const [allCourses,setAllCourses] = useState([])
+    const [isEducator,setisEducator] = useState(true);
+
+    //Fetch all Courses
+    const fetchAllCourses = async()=>{
+        setAllCourses(dummyCourses)
     }
+
+    //Fetch to calculate average rating of course
+    const calculateRating = (course)=>{
+        if(course.courseRatings.length===0){
+            return 0;
+        }
+        let totalRating=0
+        course.courseRatings.forEach(rating=>{
+            totalRating += rating.rating
+        })
+        return totalRating/course.courseRatings.length
+    }
+
+    useEffect(()=>{
+        fetchAllCourses()
+    },[])
+    // const value = {
+    //       currency,allCourses,navigate,calculateRating
+    // }
+    const value = {
+  currency,
+  allCourses,
+  navigate,
+  calculateRating,isEducator,setisEducator
+};
+
+console.log("✅ Context value exported:", value); // ✅ Now it's safe
+
 
     return(
         <AppContext.Provider value={value}>
