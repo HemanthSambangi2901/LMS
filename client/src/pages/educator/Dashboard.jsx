ab529a53bea4c670c680d863581ea69413b3776b
Samhitha <samhithabuddha@gmail.com>
2025-09-10 15:46:17 +0530

    Designed the educator page

--- client/src/pages/educator/Dashboard.jsx
+++ client/src/pages/educator/Dashboard.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React,{useContext, useState} from 'react'
+import { AppContext } from '../../context/AppContext'
 
 const Dashboard = () => {
+
+const {currency}=useContext(AppContext)
+const [dashboard,setdashboard]=useState(null)
+
+
   return (
     <div>
         <h1>Eductaor Dashboard</h1>
