aa672083a2b1ac61bfc651a98eb5e9f046c2f8fb
HemanthSambangi2901 <hemanthsambangi2901@gmail.com>
2025-09-11 00:23:15 +0530

    Completed the Frontend

--- client/src/components/educator/Footer.jsx
+++ client/src/components/educator/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className='hidden md:block h-7 w-px bg-gray-500/60'></div>
         <div>
           <p className='py-4 text-center text-xs md:text-sm text-gray-500'>
-            Copyright 2024 @Edemy.All Rights Reserved.
+            Copyright 2025 @Edemy.All Rights Reserved.
           </p>
         </div>
       </div>
